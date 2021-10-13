60619cd6cfa6fe80acca3759d7044e39c57e0d5c
jimena-rs <jimena.rico10@gmail.com>
2021-10-13 16:48:35 -0700

    part 2 complete

--- assignment_2.R
+++ assignment_2.R
@@ -33,8 +33,27 @@ neighborhoods= neighborhoods %>%
   head(20)
 
 # 2c) airbnb tibble only with top 20 neighborhoods (from neighborhoods tibble)
-airbnb_top_neighborhoods
+airbnb_top_neighborhoods <- airbnb %>%
+  filter(neighborhood %in% neighborhoods$neighborhood)
 
+# check we got only top 20 neighborhoods
+#airbnb_top_neighborhoods %>% count(neighborhood, sort = T)
+
+#2d) summary stats top 20 neighborhoods
+summary_stats_top_neighborhoods <- airbnb_top_neighborhoods %>%
+  group_by(neighborhood) %>%
+  summarize(avg_square_feet=mean(square_feet, na.rm=T),avg_price=mean(price), 
+            sd_price=sd(price), max_price=max(price), min_price=min(price)) %>%
+  arrange(desc(avg_square_feet))
+  
+#2e)  save highest average square feet (extract an element from matrix)
+highest_avg_square_ft= summary_stats_top_neighborhoods[[1,2]] 
+
+#2f) save 2nd highest average price
+summary_stats_top_neighborhoods<- summary_stats_top_neighborhoods %>% 
+  arrange(desc(avg_price))
+
+second_avg_price= summary_stats_top_neighborhoods[[2,3]] 
 
 
 
